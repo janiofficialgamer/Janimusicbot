66c8c4faaaa0176c6233bd5bef0cb4f5ee008a49
janiofficialgamer <janiofficialgamer@gmail.com>
2026-04-11 10:28:39 +0530

    Update play.py

--- AloneMusic/utils/inline/play.py
+++ AloneMusic/utils/inline/play.py
@@ -100,6 +100,12 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            
+            ],
+                [
+         InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)
+        ],
+    
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
